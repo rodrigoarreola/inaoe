e2e608edeef8abd320531600d8eea47dae3de5ed
rodrigoarreola <rodrigoarreola96@gmail.com>
2017-08-28 15:41:08 -0500

    Inicio del proyecto

---
+++ js/myscript.js
@@ -0,0 +1,37 @@
+var bandera = false;
+var status = 1;
+$(".fa-bell").click(function(e){
+    console.log("bell");
+});
+
+$(function() {
+  $('.fa').click(function() {
+    var wasPlay = $(this).hasClass('fa-play');
+
+    //Status: 1 play -> 2 stop -> 3 complete stop
+
+    console.log(status);
+    var aux= $(this); //we can use it later
+    $(aux).removeClass('fa-play fa-stop');
+    var klass = wasPlay ? 'fa-stop' : 'fa-play';
+    $(aux).addClass(klass)
+    if (status==2) {
+      $('#modal-alert').modal('show') //trigger the modal
+      $("#btn-danger-aceptar").click(function(e){
+        console.log("Click btn-danger");
+        $(aux).removeClass('fa-stop fa-play');
+        var klass = wasPlay ? 'fa-play' : 'fa-stop';
+        $(aux).addClass(klass)
+        /*
+        ... code to stop process
+
+        */
+      });
+    }
+
+    status++;
+    if (status==3) {
+      status = 1
+    }
+  });
+});
